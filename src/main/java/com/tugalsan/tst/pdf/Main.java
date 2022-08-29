17cd64b0f6a54daab946beccafddd75ccf0b3543
tugalsan karabacak <tugalsan@gmail.com>
2022-08-29 12:17:35 +0300

    api.log dep code update

--- src/main/java/com/tugalsan/tst/pdf/Main.java
+++ src/main/java/com/tugalsan/tst/pdf/Main.java
@@ -8,7 +8,7 @@ import java.util.*;
 
 public class Main {
 
-    private static TS_Log d = TS_Log.of(Main.class.getSimpleName());
+    private static TS_Log d = TS_Log.of(Main.class);
 
     public static void main(String[] args) {
         {
