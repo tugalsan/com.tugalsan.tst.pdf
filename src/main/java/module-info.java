7aee9753cef50758d0368517d8a381b52aa6d4c4
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-27 12:04:01 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.tugalsan.tst.pdf {
     requires gwt.user;
     requires com.tugalsan.api.log;
-    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.function;
     requires com.tugalsan.api.runnable;
     requires com.tugalsan.api.validator;
     requires com.tugalsan.api.tuple;
